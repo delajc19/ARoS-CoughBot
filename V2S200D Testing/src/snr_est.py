corrected snr estimation calculation: SNR_linear = (S + N - N)/N

V2S200D Testing/src/snr_est.py
@@ -1,3 +1,14 @@
+# Joseph de la Viesca
+# 
+# This script uses a very rudimentary RMS-based approach to estimate and compare the SNRs of 
+# air and bone conduction microphones. The input file is selected from the "stereo recordings"
+# folder, containing audio data recorded in the "environmental noise tests". The script is 
+# designed to process the sinusoidal tone recordings, which are 8 sec recordings comprised of
+# 4 seconds of sinusoid + noise (active) and 4 seconds of noise (inactive). 3 seconds of the 
+# active and inactive section are extracted, and the RMS for each is calculated. This is then
+# used to find an average SNR value for the bone and air conduction microphones at each frequency
+# of a desired target sound.  
+
 import os
 import numpy as np
 from scipy.io.wavfile import read
@@ -36,17 +47,36 @@ def align(audio1, audio2):
 def rms(signal):
     return np.sqrt(np.mean(signal**2))
 
-audio_dir = "..\\stereo recordings"
-filename = 'Q_ST500Hz.wav'
-pathname = os.path.join(audio_dir, filename)
+#Select audio file
+audio_dir = "..\\stereo recordings\\"
+
+if not os.path.exists(audio_dir):
+    raise FileNotFoundError(f"Directory does not exist: {os.path.abspath(audio_dir)}")
+
+#List files
+file_list = sorted(os.listdir(audio_dir))
+num_files = len(file_list)
+
+for i in range(num_files):
+    print(f"{i+1} {file_list[i]}")
+
+
+selection = int(input("Please input the index of the audio file you wish to analyze: ")) - 1
+
+#Ensure user inputs valid index
+while(selection < 0 or selection >= num_files):
+    selection = int(input(f"Invalid input, please input a value in the range [1, {num_files}]: ")) - 1
+
+filename = os.path.join(audio_dir, file_list[selection])
 
 #Load selected audio file
-Fs, audiofile = read(pathname)
+Fs, audiofile = read(filename)
 
 #Ensure user selects stereo file
 while(audiofile.ndim != 2 or audiofile.shape[1] < 2):
     raise ValueError("Expected a stereo audio file with 2 channels")
 
+print(f"File selected: {file_list[selection]}")
 
 # audiofile = audiofile[:5*Fs]
 audio_dtype = audiofile.dtype
@@ -74,13 +104,17 @@ active_end = int(active_start + 4*Fs)
 inactive_start = int(4.5*Fs)
 inactive_end = int(7.5*Fs)
 
-signal_rms = rms(bone_rec[active_start:active_end])
+mixed_rms = rms(bone_rec[active_start:active_end])
 noise_rms = rms(bone_rec[inactive_start:inactive_end])
+signal_rms = np.sqrt(np.maximum(mixed_rms**2 - noise_rms**2, 1e-12))
+signal_rms = mixed_rms #for debugging
 print(f"BCM Noise power: {20*np.log10(noise_rms):.2f} dBFS")
 snrbone_db = 20*np.log10(signal_rms/noise_rms)
 
-signal_rms = rms(air_rec[active_start:active_end])
+mixed_rms = rms(air_rec[active_start:active_end])
 noise_rms = rms(air_rec[inactive_start:inactive_end])
+signal_rms = np.sqrt(np.maximum(mixed_rms**2 - noise_rms**2, 1e-12))
+signal_rms = mixed_rms #for debugging
 print(f"ACM Noise power: {20*np.log10(noise_rms):.2f} dBFS")
 snrair_db = 20*np.log10(signal_rms/noise_rms)
 